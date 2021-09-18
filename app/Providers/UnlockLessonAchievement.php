facfbe462f746cd16abad75d83afe78da7db01ba
Warihana Gumah <warihana.gumah@thegitlab.com>
2021-09-18 17:07:34 +0000

    Unlock Lessons watched  Achievement listener done

--- app/Providers/UnlockLessonAchievement.php
+++ app/Providers/UnlockLessonAchievement.php
@@ -26,6 +26,50 @@ class UnlockLessonAchievement
      */
     public function handle(LessonWatched $event)
     {
-        //
+        //get and save user lesson
+        $lesson = $event->lesson;
+        $lesson_id = $lesson->id;
+        $user = $event->user;
+        $user_id = $user->id;
+
+        DB::table('lesson_user')->insert(
+            [
+                'user_id' => $user_id,
+                'lesson_id' => $lesson_id,
+                'watched' => '1'
+            ]
+        );
+
+        //get and count lessons user has watched
+        $lessons_watched = DB::table('lesson_user')
+            ->where('user_id', '=', $user_id)
+            ->count();
+
+        $achievement_name = '';
+
+        if($lessons_watched == 1)
+        {
+            $achievement_name = "First Lesson Watched";
+        }else if($lessons_watched >= 5 && $lessons_watched < 10)
+        {
+            $achievement_name = "5 Lessons Watched";
+
+        }else if($lessons_watched >= 10 && $lessons_watched < 25)
+        {
+            $achievement_name = "10 Lessons Watched";
+
+        }else if($lessons_watched >= 25 && $lessons_watched < 50)
+        {
+            $achievement_name = "25 Lessons Watched";
+
+        }else if($lessons_watched >= 50)
+        {
+            $achievement_name = "50 Lessons Watched";
+
+        }
+
+        //fire AchievementUnlocked event
+        event(new AchievementUnlocked($achievement_name, $user));
+
     }
 }
